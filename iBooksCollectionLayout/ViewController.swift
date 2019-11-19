5d7db082d6b8126f97cb5aaf95b6bad194b6c3e6
tromg <tromg1@gmail.com>
2019-11-19 11:31:10 +0300

    Initial Commit

---
+++ iBooksCollectionLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBooksCollectionLayout
+//
+//  Created by Evgeny Petrov on 19.11.2019.
+//  Copyright © 2019 tromg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
